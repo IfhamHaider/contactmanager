aa63be3c18e1b777346260875573023096d3b962
ifham haider <ifhamhaider14@gmail.com>
2020-06-27 14:38:52 +0500

    Updates

---
+++ precache-manifest.baa328f231967c67a66dff9a39088578.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9664d15cbc06beea2c4e8de0be1813dd",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "2a07ce444f3dc92cb2a3",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2395ee6a01e9d723bdf9",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2a07ce444f3dc92cb2a3",
+    "url": "/contactmanager/static/js/2.f3e00213.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.f3e00213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2395ee6a01e9d723bdf9",
+    "url": "/contactmanager/static/js/main.1e4afc74.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
